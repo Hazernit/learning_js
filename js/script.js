da913fc542b08e8e4f8ef7518107d7e1b82ff0b9
Dmitriy <dima.k.55555@gmail.com>
2023-03-19 22:46:51 +0300

    add function_learning.js| lesson 25

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
 "use strict";
+
+// Функции - function declaration
+
+
+function showFirstMessage() {
+    console.log("Hello World");
+}
+showFirstMessage();
+
+let num = 20
+function showFirstMessage(text) {
+    console.log(text);
+    let num = 10;
+    console.log(num)
+}
+
+showFirstMessage("text");
+console.log(num)
+
+// Замыкание функции в JavaScript 
+// Замыкание функции - это сама функция вместе
+// со всеми внешними переменными которые ей доступны
+
+function calc(a, b) {
+    return (a + b);
+}
+console.log(calc(4, 3));
+
+function ret() {
+    let num = 50;
+
+    // код  какой-то
+
+    return num
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+// Стрелочные функции
